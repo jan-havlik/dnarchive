49e792d1050f8f16bb5fbaf8537a7a1d29c67c4a
Patrik Kaura <patrikkaura@gmail.com>
2023-12-03 14:03:45 +0100

    feat: fix listing + change input parames

--- server/manager/adapter/client.ts
+++ server/manager/adapter/client.ts
@@ -2,21 +2,6 @@ import { API_URL } from "@config";
 import { Zodios } from "@zodios/core";
 import { z } from "zod";
 
-const analysisResultSchema = z.object({
-  g4_hunter: z.array(
-    z.object({
-      id: z.number(),
-      position: z.number(),
-      length: z.number(),
-      score: z.number(),
-      sequence: z.string(),
-      sub_score: z.number(),
-    })
-  ),
-});
-
-export type AnalysisResult = z.infer<typeof analysisResultSchema>;
-
 export const api = new Zodios(
   API_URL,
   [
@@ -30,22 +15,23 @@ export const api = new Zodios(
         { name: "end", type: "Query", schema: z.number() },
       ],
       response: z.object({
-        analysis: z.object({
-          g4_hunter: z.array(
-            z.object({
-              id: z.number(),
-              position: z.number(),
-              length: z.number(),
-              score: z.number(),
-              sequence: z.string(),
-              sub_score: z.number(),
-            })
-          ),
-        }),
+        analysis: z.array(
+          z.object({
+            abs_score: z.number(),
+            chromosome: z.string(),
+            length: z.number(),
+            position: z.number(),
+            score: z.number(),
+            sequence: z.string(),
+            sub_score: z.number(),
+            threshold: z.number(),
+          })
+        ),
         sequence: z.array(z.string()),
       }),
       status: 200,
     },
+
     {
       method: "get",
       path: "/analysis",
@@ -62,7 +48,17 @@ export const api = new Zodios(
         { name: "g4-threshold", type: "Query", schema: z.number().optional() },
         { name: "g4-window", type: "Query", schema: z.number().optional() },
       ],
-      response: analysisResultSchema,
+      response: z.array(
+        z.object({
+          abs_score: z.number(),
+          chromosome: z.string(),
+          length: z.number(),
+          position: z.number(),
+          score: z.number(),
+          sequence: z.string(),
+          sub_score: z.number(),
+        })
+      ),
       status: 200,
     },
     {
@@ -75,8 +71,7 @@ export const api = new Zodios(
           name: z.string(),
           ref_seq: z.string(),
           length: z.number(),
-          gc_content: z.number(),
-          gc_skew: z.number(),
+          g4_count: z.number(),
           updated_at: z.string(),
         })
       ),
